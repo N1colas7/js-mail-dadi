72eadafee8d79e98a2ce65382726bf0a5233641e
N1colas7 <nicolasnd9897@gmail.com>
2022-11-30 16:52:20 +0100

    finish ex email

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,7 +1,7 @@
 const bottone = document.getElementById('conferma-email');
 bottone.addEventListener('click', function()
 {
-    const guests = ( "nicolasd.pippo@gmail.com", "marco.pippo@gmail.com", "paolo.pippo@gmail.com", "fabrizio.pippo@gmail.com" )
+    const guests = [ "nicolasd.pippo@gmail.com", "marco.pippo@gmail.com", "paolo.pippo@gmail.com", "fabrizio.pippo@gmail.com" ]
     let user = document.getElementById('email').value;
     let found = false;
     for (let i = 0; i < guests.length; i++){
