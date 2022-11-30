82b0b9f24e82bb074164c267ff550c027bdc4505
N1colas7 <nicolasnd9897@gmail.com>
2022-11-30 16:03:32 +0100

    const button

---
+++ mail/js/script.js
@@ -0,0 +1,6 @@
+const bottone = document.getElementById('conferma-email');
+bottone.addEventListener('click', function()
+{
+    const email = ( "nicolasd.pippo@gmail.com", "marco.pippo@gmail.com", "paolo.pippo@gmail.com", "fabrizio.pippo@gmail.com" )
+
+})
