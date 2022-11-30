dd8d049e2ebe8442e2f3908b8d64b607daae93d9
N1colas7 <nicolasnd9897@gmail.com>
2022-11-30 16:35:39 +0100

    to fix

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,6 +1,18 @@
 const bottone = document.getElementById('conferma-email');
 bottone.addEventListener('click', function()
 {
-    const email = ( "nicolasd.pippo@gmail.com", "marco.pippo@gmail.com", "paolo.pippo@gmail.com", "fabrizio.pippo@gmail.com" )
-
+    const guests = ( "nicolasd.pippo@gmail.com", "marco.pippo@gmail.com", "paolo.pippo@gmail.com", "fabrizio.pippo@gmail.com" )
+    let user = document.getElementById('email').value;
+    let found = false;
+    for (let i = 0; i < guests.length; i++){
+        const guestName = guests[i];
+        if (guestName == user) {
+            found = true;
+        }
+    }
+    if (found) {
+        console.log("Puoi partecipare"); 
+    } else {
+        console.log("Non puoi partecipare");
+    }
 })
